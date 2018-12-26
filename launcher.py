0f62a13dfa854ec0e9a5a4a360ed50038a39bd06
Quantomistro3178 <m.saifi3178@gmail.com>
2018-12-26 20:00:19 +0800

    remove audio capability

--- launcher.py
+++ launcher.py
@@ -22,11 +22,9 @@ except ImportError:
 REQS_DIR = "lib"
 sys.path.insert(0, REQS_DIR)
 REQS_TXT = "requirements.txt"
-REQS_NO_AUDIO_TXT = "requirements_no_audio.txt"
-FFMPEG_BUILDS_URL = "https://ffmpeg.zeranoe.com/builds/"
 
 INTRO = ("==========================\n"
-         "Red Discord Bot - Launcher\n"
+         "    TCG Bot - Launcher    \n"
          "==========================\n")
 
 IS_WINDOWS = os.name == "nt"
@@ -35,29 +33,19 @@ IS_64BIT = platform.machine().endswith("64")
 INTERACTIVE_MODE = not len(sys.argv) > 1  # CLI flags = non-interactive
 PYTHON_OK = sys.version_info >= (3, 5)
 
-FFMPEG_FILES = {
-    "ffmpeg.exe"  : "e0d60f7c0d27ad9d7472ddf13e78dc89",
-    "ffplay.exe"  : "d100abe8281cbcc3e6aebe550c675e09",
-    "ffprobe.exe" : "0e84b782c0346a98434ed476e937764f"
-}
-
-
 def parse_cli_arguments():
-    parser = argparse.ArgumentParser(description="Red - Discord Bot's launcher")
+    parser = argparse.ArgumentParser(description="TCG Bot's launcher")
     parser.add_argument("--start", "-s",
-                        help="Starts Red",
+                        help="Starts the bot",
                         action="store_true")
     parser.add_argument("--auto-restart",
-                        help="Autorestarts Red in case of issues",
+                        help="Autorestarts the bot in case of issues",
                         action="store_true")
-    parser.add_argument("--update-red",
-                        help="Updates Red (git)",
+    parser.add_argument("--update-bot",
+                        help="Updates bot (git)",
                         action="store_true")
     parser.add_argument("--update-reqs",
-                        help="Updates requirements (w/ audio)",
-                        action="store_true")
-    parser.add_argument("--update-reqs-no-audio",
-                        help="Updates requirements (w/o audio)",
+                        help="Updates requirements",
                         action="store_true")
     parser.add_argument("--repair",
                         help="Issues a git reset --hard",
@@ -65,7 +53,7 @@ def parse_cli_arguments():
     return parser.parse_args()
 
 
-def install_reqs(audio):
+def install_reqs():
     remove_reqs_readonly()
     interpreter = sys.executable
 
@@ -73,7 +61,7 @@ def install_reqs(audio):
         print("Python interpreter not found.")
         return
 
-    txt = REQS_TXT if audio else REQS_NO_AUDIO_TXT
+    txt = REQS_TXT
 
     args = [
         interpreter, "-m",
@@ -117,7 +105,7 @@ def update_pip():
         print("\nAn error occurred and pip might not have been updated.")
 
 
-def update_red():
+def update_bot():
     try:
         code = subprocess.call(("git", "pull", "--ff-only"))
     except FileNotFoundError:
@@ -125,14 +113,14 @@ def update_red():
               "the PATH environment variable like requested in the guide.")
         return
     if code == 0:
-        print("\nRed has been updated")
+        print("\nTCG Bot has been updated")
     else:
-        print("\nRed could not update properly. If this is caused by edits "
+        print("\nThe bot was unable to update properly. If this is caused by edits "
               "you have made to the code you can try the repair option from "
               "the Maintenance submenu")
 
 
-def reset_red(reqs=False, data=False, cogs=False, git_reset=False):
+def reset_bot(reqs=False, data=False, cogs=False, git_reset=False):
     if reqs:
         try:
             shutil.rmtree(REQS_DIR, onerror=remove_readonly)
@@ -165,63 +153,15 @@ def reset_red(reqs=False, data=False, cogs=False, git_reset=False):
     if git_reset:
         code = subprocess.call(("git", "reset", "--hard"))
         if code == 0:
-            print("Red has been restored to the last local commit.")
+            print("TCG Bot has been restored to the last local commit.")
         else:
             print("The repair has failed.")
 
-
-def download_ffmpeg(bitness):
-    clear_screen()
-    repo = "https://github.com/Twentysix26/Red-DiscordBot/raw/master/"
-    verified = []
-
-    if bitness == "32bit":
-        print("Please download 'ffmpeg 32bit static' from the page that "
-              "is about to open.\nOnce done, open the 'bin' folder located "
-              "inside the zip.\nThere should be 3 files: ffmpeg.exe, "
-              "ffplay.exe, ffprobe.exe.\nPut all three of them into the "
-              "bot's main folder.")
-        time.sleep(4)
-        webbrowser.open(FFMPEG_BUILDS_URL)
-        return
-
-    for filename in FFMPEG_FILES:
-        if os.path.isfile(filename):
-            print("{} already present. Verifying integrity... "
-                  "".format(filename), end="")
-            _hash = calculate_md5(filename)
-            if _hash == FFMPEG_FILES[filename]:
-                verified.append(filename)
-                print("Ok")
-                continue
-            else:
-                print("Hash mismatch. Redownloading.")
-        print("Downloading {}... Please wait.".format(filename))
-        with urllib.request.urlopen(repo + filename) as data:
-            with open(filename, "wb") as f:
-                f.write(data.read())
-        print("Download completed.")
-
-    for filename, _hash in FFMPEG_FILES.items():
-        if filename in verified:
-            continue
-        print("Verifying {}... ".format(filename), end="")
-        if not calculate_md5(filename) != _hash:
-            print("Passed.")
-        else:
-            print("Hash mismatch. Please redownload.")
-
-    print("\nAll files have been downloaded.")
-
-
 def verify_requirements():
-    sys.path_importer_cache = {} # I don't know if the cache reset has any
-    basic = find_spec("discord") # side effect. Without it, the lib folder
-    audio = find_spec("nacl")    # wouldn't be seen if it didn't exist
-    if not basic:                # when the launcher was started
+    sys.path_importer_cache = {} # I don't know if the cache reset has any side effect.
+    basic = find_spec("discord") # Without it, the lib folder wouldn't be seen if it
+    if not basic:                # didn't exist when the launcher was started
         return None
-    elif not audio:
-        return False
     else:
         return True
 
@@ -242,27 +182,11 @@ def requirements_menu():
     while True:
         print(INTRO)
         print("Main requirements:\n")
-        print("1. Install basic + audio requirements (recommended)")
-        print("2. Install basic requirements")
-        if IS_WINDOWS:
-            print("\nffmpeg (required for audio):")
-            print("3. Install ffmpeg 32bit")
-            if IS_64BIT:
-                print("4. Install ffmpeg 64bit (recommended on Windows 64bit)")
+        print("1. Install basic requirements")
         print("\n0. Go back")
         choice = user_choice()
         if choice == "1":
-            install_reqs(audio=True)
-            wait()
-        elif choice == "2":
-            install_reqs(audio=False)
-            wait()
-        elif choice == "3" and IS_WINDOWS:
-            download_ffmpeg(bitness="32bit")
-            wait()
-        elif choice == "4" and (IS_WINDOWS and IS_64BIT):
-            download_ffmpeg(bitness="64bit")
-            wait()
+            install_reqs()
         elif choice == "0":
             break
         clear_screen()
@@ -276,35 +200,31 @@ def update_menu():
         if reqs is None:
             status = "No requirements installed"
         elif reqs is False:
-            status = "Basic requirements installed (no audio)"
-        else:
-            status = "Basic + audio requirements installed"
+            status = "Basic requirements installed"
         print("Status: " + status + "\n")
         print("Update:\n")
-        print("Red:")
-        print("1. Update Red + requirements (recommended)")
-        print("2. Update Red")
+        print("1. Update bot + requirements (recommended)")
+        print("2. Update bot")
         print("3. Update requirements")
-        print("\nOthers:")
         print("4. Update pip (might require admin privileges)")
         print("\n0. Go back")
         choice = user_choice()
         if choice == "1":
-            update_red()
+            update_bot()
             print("Updating requirements...")
             reqs = verify_requirements()
             if reqs is not None:
-                install_reqs(audio=reqs)
+                install_reqs()
             else:
                 print("The requirements haven't been installed yet.")
             wait()
         elif choice == "2":
-            update_red()
+            update_bot()
             wait()
         elif choice == "3":
             reqs = verify_requirements()
             if reqs is not None:
-                install_reqs(audio=reqs)
+                install_reqs()
             else:
                 print("The requirements haven't been installed yet.")
             wait()
@@ -321,7 +241,7 @@ def maintenance_menu():
     while True:
         print(INTRO)
         print("Maintenance:\n")
-        print("1. Repair Red (discards code changes, keeps data intact)")
+        print("1. Repair bot (discards code changes, keeps data intact)")
         print("2. Wipe 'data' folder (all settings, cogs' data...)")
         print("3. Wipe 'lib' folder (all local requirements / local installed"
               " python packages)")
@@ -332,38 +252,38 @@ def maintenance_menu():
             print("Any code modification you have made will be lost. Data/"
                   "non-default cogs will be left intact. Are you sure?")
             if user_pick_yes_no():
-                reset_red(git_reset=True)
+                reset_bot(git_reset=True)
                 wait()
         elif choice == "2":
             print("Are you sure? This will wipe the 'data' folder, which "
                   "contains all your settings and cogs' data.\nThe 'cogs' "
                   "folder, however, will be left intact.")
             if user_pick_yes_no():
-                reset_red(data=True)
+                reset_bot(data=True)
                 wait()
         elif choice == "3":
-            reset_red(reqs=True)
+            reset_bot(reqs=True)
             wait()
         elif choice == "4":
-            print("Are you sure? This will wipe ALL your Red's installation "
+            print("Are you sure? This will wipe ALL installations. "
                   "data.\nYou'll lose all your settings, cogs and any "
                   "modification you have made.\nThere is no going back.")
             if user_pick_yes_no():
-                reset_red(reqs=True, data=True, cogs=True, git_reset=True)
+                reset_bot(reqs=True, data=True, cogs=True, git_reset=True)
                 wait()
         elif choice == "0":
             break
         clear_screen()
 
 
-def run_red(autorestart):
+def run_bot(autorestart):
     interpreter = sys.executable
 
     if interpreter is None: # This should never happen
         raise RuntimeError("Couldn't find Python's interpreter")
 
     if verify_requirements() is None:
-        print("You don't have the requirements to start Red. "
+        print("You don't have the requirements to start the bot. "
               "Install them from the launcher.")
         if not INTERACTIVE_MODE:
             exit(1)
@@ -380,13 +300,13 @@ def run_red(autorestart):
             if code == 0:
                 break
             elif code == 26:
-                print("Restarting Red...")
+                print("Restarting bot...")
                 continue
             else:
                 if not autorestart:
                     break
 
-    print("Red has been terminated. Exit code: %d" % code)
+    print("Process has been terminated. Exit code: %d" % code)
 
     if INTERACTIVE_MODE:
         wait()
@@ -443,15 +363,15 @@ def calculate_md5(filename):
 
 
 def create_fast_start_scripts():
-    """Creates scripts for fast boot of Red without going
+    """Creates scripts for fast boot without going
     through the launcher"""
     interpreter = sys.executable
     if not interpreter:
         return
 
     call = "\"{}\" launcher.py".format(interpreter)
-    start_red = "{} --start".format(call)
-    start_red_autorestart = "{} --start --auto-restart".format(call)
+    start_bot = "{} --start".format(call)
+    start_bot_autorestart = "{} --start --auto-restart".format(call)
     modified = False
 
     if IS_WINDOWS:
@@ -466,12 +386,12 @@ def create_fast_start_scripts():
         else:
             ext = ".command"
 
-    start_red             = ccd + start_red             + pause
-    start_red_autorestart = ccd + start_red_autorestart + pause
+    start_bot             = ccd + start_bot             + pause
+    start_bot_autorestart = ccd + start_bot_autorestart + pause
 
     files = {
-        "start_red"             + ext : start_red,
-        "start_red_autorestart" + ext : start_red_autorestart
+        "start_bot"             + ext : start_bot,
+        "start_bot_autorestart" + ext : start_bot_autorestart
     }
 
     if not IS_WINDOWS:
@@ -495,7 +415,7 @@ def main():
     has_git = is_git_installed()
     is_git_installation = os.path.isdir(".git")
     if IS_WINDOWS:
-        os.system("TITLE Red Discord Bot - Launcher")
+        os.system("TITLE TCG Bot - Launcher")
     clear_screen()
 
     try:
@@ -507,29 +427,28 @@ def main():
         print(INTRO)
 
         if not is_git_installation:
-            print("WARNING: It doesn't look like Red has been "
+            print("WARNING: It doesn't look like the bot has been "
                   "installed with git.\nThis means that you won't "
                   "be able to update and some features won't be working.\n"
                   "A reinstallation is recommended. Follow the guide "
-                  "properly this time:\n"
-                  "https://twentysix26.github.io/Red-Docs/\n")
+                  "properly this time.")
 
         if not has_git:
             print("WARNING: Git not found. This means that it's either not "
                   "installed or not in the PATH environment variable like "
                   "requested in the guide.\n")
 
-        print("1. Run Red /w autorestart in case of issues")
-        print("2. Run Red")
+        print("1. Run bot /w autorestart in case of issues")
+        print("2. Run bot")
         print("3. Update")
         print("4. Install requirements")
         print("5. Maintenance (repair, reset...)")
         print("\n0. Quit")
         choice = user_choice()
         if choice == "1":
-            run_red(autorestart=True)
+            run_bot(autorestart=True)
         elif choice == "2":
-            run_red(autorestart=False)
+            run_bot(autorestart=False)
         elif choice == "3":
             update_menu()
         elif choice == "4":
@@ -548,26 +467,24 @@ if __name__ == '__main__':
     # Sets current directory to the script's
     os.chdir(dirname)
     if not PYTHON_OK:
-        print("Red needs Python 3.5 or superior. Install the required "
+        print("Python 3.5 or superior is needed. Install the required "
               "version.\nPress enter to continue.")
         if INTERACTIVE_MODE:
             wait()
         exit(1)
     if pip is None:
-        print("Red cannot work without the pip module. Please make sure to "
+        print("The bot cannot work without the pip module. Please make sure to "
               "install Python without unchecking any option during the setup")
         wait()
         exit(1)
     if args.repair:
-        reset_red(git_reset=True)
-    if args.update_red:
-        update_red()
+        reset_bot(git_reset=True)
+    if args.update_bot:
+        update_bot()
     if args.update_reqs:
-        install_reqs(audio=True)
-    elif args.update_reqs_no_audio:
-        install_reqs(audio=False)
+        install_reqs()
     if INTERACTIVE_MODE:
         main()
     elif args.start:
-        print("Starting Red...")
-        run_red(autorestart=args.auto_restart)
+        print("Starting TCG Bot...")
+        run_bot(autorestart=args.auto_restart)
