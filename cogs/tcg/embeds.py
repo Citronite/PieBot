732f959aae8f819380999f0904eabf910ad06e24
Quantomistro3178 <m.saifi3178@gmail.com>
2019-02-12 02:09:17 +0800

    Create empty TCG Embed classes

---
+++ cogs/tcg/embeds.py
@@ -0,0 +1,16 @@
+from discord import Embed
+
+class CardInfoEmbed(Embed):
+    pass
+
+class ItemInfoEmbed(Embed):
+    pass
+
+class InventoryEmbed(Embed):
+    pass
+
+class PlayerCardEmbed(Embed):
+    pass
+
+class ClanInfoEmbed(Embed):
+    pass
